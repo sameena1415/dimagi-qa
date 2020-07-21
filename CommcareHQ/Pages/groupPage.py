df758d2544ecfd237cc7128e49aac464d8059b69
Kiran Tripathy <kiran6.kt@gmail.com>
2020-07-21 12:10:15 +0530

    Update groupPage.py

--- CommcareHQ/Pages/groupPage.py
+++ CommcareHQ/Pages/groupPage.py
@@ -13,6 +13,9 @@ class GroupPage:
         self.group_name_id = "id_group_name"
         self.add_group_button = "//button[@type='submit' and @class='btn btn-primary']"
         self.group_menu_xpath = "//a[@data-title='Groups']"
+        self.users_drop_down_xpath = "//span[@class='selection']"
+        self.select_user_xpath = "//li[text()='"+UserInputs.mobile_worker_username+"']"
+        self.update_button_id = "submit-id -submit"
 
     def click_group_menu(self):
         self.driver.find_element_by_xpath(self.group_menu_xpath).click()
@@ -29,21 +32,21 @@ class GroupPage:
         time.sleep(2)
 
     def click_on_users_dropdown(self):
-        self.driver.find_element_by_xpath("//span[@class='selection']").click()
-        time.sleep (2)
+        self.driver.find_element_by_xpath(self.users_drop_down_xpath).click()
+        time.sleep(2)
 
     def add_user_to_group(self):
-        time.sleep (2)
-        self.driver.find_element_by_xpath("//li[text()='"+UserInputs.mobile_worker_username+"']").click()
-        time.sleep (2)
+        time.sleep(2)
+        self.driver.find_element_by_xpath(self.select_user_xpath).click()
+        time.sleep(2)
 
     def update_group(self):
-        self.driver.find_element_by_id("submit-id -submit").click ( )
+        self.driver.find_element_by_id(self.update_button_id).click()
         time.sleep(2)
 
     def edit_existing_group(self):
         self.click_group_menu()
-        self.driver.find_element_by_link_text (UserInputs.group_name).click ( )
+        self.driver.find_element_by_link_text(UserInputs.group_name).click()
 
 
 
