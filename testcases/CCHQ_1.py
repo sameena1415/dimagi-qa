852468cbf17ea50cd1f79eb2b6d49895e86495ab
crimsonsun25 <65016488+crimsonsun25@users.noreply.github.com>
2020-10-21 12:06:30 +0530

    Update CCHQ_1.py

--- testcases/CCHQ_1.py
+++ testcases/CCHQ_1.py
@@ -1,5 +1,3 @@
-# Import the libraries and packages we will need and create a Unit Test Class that will have
-# a setup, a teardown and our first test method (or test case).
 import unittest
 from pageobjects.export_data import Export_Data
 from pageobjects.login import Login
@@ -29,41 +27,41 @@ class TestCCHQ(unittest.TestCase):
             time.sleep(2)
             export.del_form_exports()
             time.sleep(2)
-            #export.add_form_exports()
+            export.add_form_exports()
             time.sleep(2)
-            #export.form_exports()
+            export.form_exports()
             time.sleep(2)
-            #export.validate_downloaded_form_exports()
+            export.validate_downloaded_form_exports()
             time.sleep(2)
-            #export.add_case_exports()
+            export.add_case_exports()
             time.sleep(2)
-            #export.case_exports()
+            export.case_exports()
             time.sleep(2)
-            #export.validate_downloaded_case_exports()
+            export.validate_downloaded_case_exports()
             time.sleep(2)
-            #export.sms_exports()
+            export.sms_exports()
             time.sleep(2)
-            #export.daily_saved_exports()
+            export.daily_saved_exports()
             time.sleep(2)
-            #export.excel_dashboard_integration_form()
+            export.excel_dashboard_integration_form()
             time.sleep(2)
-            #export.excel_dashboard_integration_case()
+            export.excel_dashboard_integration_case()
             time.sleep(2)
-            #export.powerBI_tableau_integration_form()
+            export.powerBI_tableau_integration_form()
             time.sleep(2)
-            #export.powerBI_tableau_integration_case()
+            export.powerBI_tableau_integration_case()
             time.sleep(2)
-            #export.manage_forms()
+            export.manage_forms()
 
-        #except Exception as e:
-        #    print(e)
+        except Exception as e:
+            print(e)
         finally:
             print("We are in!")
             time.sleep(2)
 
 
-    # def tearDown(self):
-    # self.driver.instance.quit()
+     def tearDown(self):
+     self.driver.instance.quit()
 
 
 if __name__ == '__main__':
