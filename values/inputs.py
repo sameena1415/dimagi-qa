bafa9e71b92a80a3dfae4d502c71b36f9c72f308
crimsonsun25 <65016488+crimsonsun25@users.noreply.github.com>
2020-10-21 12:01:24 +0530

    Create inputs.py

---
+++ values/inputs.py
@@ -0,0 +1,15 @@
+#Login credentials
+base_url = "https://www.commcarehq.org/accounts/login/"
+login_username = ""
+login_password = ""
+form_export_name = "CCHQ Smoke Tests Form Export"
+case_export_name = "CCHQ Smoke Tests Case Export"
+dashboard_feed_form = "CCHQ Smoke Tests Dashboard Form feed"
+dashboard_feed_case = "CCHQ Smoke Tests Dashboard Case feed"
+odata_feed_form = "CCHQ Smoke Tests Odata Form feed"
+odata_feed_case = "CCHQ Smoke Tests Odata Case feed"
+text_no_exports_to_display = "No Exports to display"
+
+download_path="C:/Users/DSI/Downloads"
+
+
