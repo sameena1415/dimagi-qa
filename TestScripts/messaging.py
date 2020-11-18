b8c0187a052b943dafd329991ae029952d092553
kiran-tripathy <ktripathy@dimagi.com>
2020-11-18 15:52:34 +0530

    uncommented tests

--- TestScripts/messaging.py
+++ TestScripts/messaging.py
@@ -6,42 +6,42 @@ from TestBase.environmentSetupPage import EnvironmentSetup
 
 class MessagingTests(EnvironmentSetup):
 
-    # def test_01_dashboard(self):
-    #     driver = self.driver
-    #     menu = HomePage(driver)
-    #     msg = MessagingPage(driver)
-    #     menu.messaging_menu()
-    #     msg.open_dashboard_page()
+    def test_01_dashboard(self):
+        driver = self.driver
+        menu = HomePage(driver)
+        msg = MessagingPage(driver)
+        menu.messaging_menu()
+        msg.open_dashboard_page()
 
-    # def test_02_compose_sms(self):
-    #     driver = self.driver
-    #     menu = HomePage(driver)
-    #     msg = MessagingPage(driver)
-    #     menu.messaging_menu()
-    #     msg.compose_sms()
+    def test_02_compose_sms(self):
+        driver = self.driver
+        menu = HomePage(driver)
+        msg = MessagingPage(driver)
+        menu.messaging_menu()
+        msg.compose_sms()
 
-    # def test_03_broadcast(self):
-    #     driver = self.driver
-    #     menu = HomePage(driver)
-    #     msg = MessagingPage(driver)
-    #     menu.messaging_menu()
-    #     msg.send_broadcast_message()
+    def test_03_broadcast(self):
+        driver = self.driver
+        menu = HomePage(driver)
+        msg = MessagingPage(driver)
+        menu.messaging_menu()
+        msg.send_broadcast_message()
 
-    # def test_04_create_cond_alert(self):
-    #     driver = self.driver
-    #     menu = HomePage(driver)
-    #     msg = MessagingPage(driver)
-    #     menu.messaging_menu()
-    #     msg.create_cond_alert()
-    #
-    # def test_05_cond_alert_bulk_upload(self):
-    #     driver = self.driver
-    #     menu = HomePage(driver)
-    #     msg = MessagingPage(driver)
-    #     menu.messaging_menu()
-    #     msg.cond_alert_download()
-    #     msg.cond_alert_upload()
-    #
+    def test_04_create_cond_alert(self):
+        driver = self.driver
+        menu = HomePage(driver)
+        msg = MessagingPage(driver)
+        menu.messaging_menu()
+        msg.create_cond_alert()
+
+    def test_05_cond_alert_bulk_upload(self):
+        driver = self.driver
+        menu = HomePage(driver)
+        msg = MessagingPage(driver)
+        menu.messaging_menu()
+        msg.cond_alert_download()
+        msg.cond_alert_upload()
+
     def test_06_keyword_creation(self):
         driver = self.driver
         menu = HomePage(driver)
@@ -50,26 +50,26 @@ class MessagingTests(EnvironmentSetup):
         # msg.add_keyword_trigger()
         msg.add_structured_keyword_trigger()
 
-    # def test_07_chats(self):
-    #     driver = self.driver
-    #     menu = HomePage(driver)
-    #     msg = MessagingPage(driver)
-    #     menu.messaging_menu()
-    #     msg.chat_page()
+    def test_07_chats(self):
+        driver = self.driver
+        menu = HomePage(driver)
+        msg = MessagingPage(driver)
+        menu.messaging_menu()
+        msg.chat_page()
 
-    # def test_08_general_settings(self):
-    #     driver = self.driver
-    #     menu = HomePage(driver)
-    #     msg = MessagingPage(driver)
-    #     menu.messaging_menu()
-    #     msg.general_settings_page()
+    def test_08_general_settings(self):
+        driver = self.driver
+        menu = HomePage(driver)
+        msg = MessagingPage(driver)
+        menu.messaging_menu()
+        msg.general_settings_page()
 
-    # def test_09_languages(self):
-    #     driver = self.driver
-    #     menu = HomePage(driver)
-    #     msg = MessagingPage(driver)
-    #     menu.messaging_menu()
-    #     msg.languages_page()
+    def test_09_languages(self):
+        driver = self.driver
+        menu = HomePage(driver)
+        msg = MessagingPage(driver)
+        menu.messaging_menu()
+        msg.languages_page()
 
 
 
