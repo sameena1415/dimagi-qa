b512195b94c8d9488edaca66e649f63bce243d4c
kiran-tripathy <ktripathy@dimagi.com>
2020-11-18 15:50:48 +0530

    corrected errors for messaging

--- Pages/messagingPage.py
+++ Pages/messagingPage.py
@@ -3,8 +3,10 @@ import glob
 from pathlib import Path
 import os
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+
 from UserInputs.generateUserInputs import fetch_random_string
-from UserInputs.userInputsData import UserInputsData
+from selenium.webdriver.support import expected_conditions as ec
 
 
 class MessagingPage:
@@ -26,10 +28,8 @@ class MessagingPage:
         self.add_broadcast = "//div[@class='btn-group']"
         self.broadcast_name = "//input[@name='schedule-schedule_name']"
         self.recipients = "(//ul[@class='select2-selection__rendered'])[1]"
-        self.select_users_recipients = "(//ul[@class='select2-results__options']/li)[1]"
-        self.user_recipient = "(//span[@class='select2 select2-container " \
-                              "select2-container--default select2-container--below'])[2]"
-        self.select_value_dropdown = "//li[text()='" + "username_" + fetch_random_string() + "']"
+        self.user_recipient = "(//span[@class='select2-selection select2-selection--multiple'])[2]"
+        self.select_value_dropdown = "(//ul[@class='select2-results__options']/li)[2]"
         self.broadcast_message = "(//textarea[@data-bind='value: nonTranslatedMessage'])[2]"
         self.send_broadcast = "//button[@data-bind='text: saveBroadcastText()']"
         self.broadcast_created = "//a[text()='" + "broadcast_" + fetch_random_string() + "']"
@@ -40,30 +40,32 @@ class MessagingPage:
         self.continue_button_basic_tab = "//button[@data-bind='click: handleBasicNavContinue, enable: basicTabValid']"
         self.case_type = "//select[@data-bind='value: caseType']"
         self.case_type_option_value = "//option[@value='case']"
-        self.continue_button_rule_tab =  "//button[@data-bind='click: handleRuleNavContinue, enable: ruleTabValid']"
+        self.continue_button_rule_tab = "//button[@data-bind='click: handleRuleNavContinue, enable: ruleTabValid']"
         self.cond_alert_created = "//a[text()='" + "cond_alert_" + fetch_random_string() + "']"
+        self.select_recipient_type = "(//ul[@id='select2-id_schedule-recipient_types-results']/li)[1]"
         # Condition Alerts : Download and Upload
         self.bulk_upload_button = "Bulk Upload SMS Alert Content"
         self.download = "Download SMS alert content"
         self.choose_file = "//input[@name='bulk_upload_file']"
-        self.upload = "Upload SMS alert content"
+        self.upload = "//button[@class='btn btn-primary disable-on-submit']"
+        self.upload_success_message = "//div[@class='alert alert-margin-top fade in alert-success']"
         # Keywords
         self.keywords = "Keywords"
         self.add_keyword = "Add Keyword"
         self.keyword_name = "//input[@name='keyword']"
         self.keyword_description = "//input[@name='description']"
         self.keyword_message = "//input[@name='sender_message']"
-        self.keyword_created = "//a[text()='" + "keyword_" + fetch_random_string() + "']"
+        self.keyword_created = "//a[text()='" + "KEYWORD_" + fetch_random_string().upper() + "']"
         self.add_structured_keyword = "Add Structured Keyword"
-        self.keyword_survey = "(//span[@class='select2-selection__rendered'])[1]"
+        self.keyword_survey = "(//span[@class='select2-selection select2-selection--single'])[1]"
         self.survey_option_select = "(//li[@class='select2-results__option'])[2]"
-        self.structured_keyword_created = "//a[text()='" + "structured_keyword_" + fetch_random_string() + "']"
+        self.structured_keyword_created = "//a[text()='" + "STRUCTURED_KEYWORD_" + fetch_random_string().upper() + "']"
         # Chat
         self.chat = "Chat"
         self.contact_table = "contact_list"
         # SMS Connectivity
         self.sms_connectivity = "SMS Connectivity"
-        self.add_gateway= "(//button[@class='btn btn-primary'])[1]"
+        self.add_gateway = "(//button[@class='btn btn-primary'])[1]"
         self.gateway_name = "//input[@name='name']"
         self.host_and_port = "//input[@name='host_and_port']"
         self.username = "//input[@name='user_name']"
@@ -90,133 +92,147 @@ class MessagingPage:
     def open_dashboard_page(self):
         time.sleep(2)
         assert True == self.driver.find_element(By.XPATH, self.dashboard_elements).is_displayed()
+        print("Messaging dashboard loaded successfully!")
 
     def compose_sms(self):
         self.driver.find_element(By.LINK_TEXT, self.compose_sms_menu).click()
-        self.driver.find_element(By.XPATH, self.recipients_textarea).send_keys("group")
-        self.driver.find_element(By.XPATH, self.select_recipient).click()
-        self.driver.find_element(By.XPATH, self.message_textarea).send_keys("Test Message")
+        self.driver.find_element(By.XPATH, self.recipients_textarea).send_keys("[send to all]")
+        time.sleep(2)
+        self.driver.find_element(By.XPATH, self.message_textarea).send_keys("sms_" + fetch_random_string())
         self.driver.find_element(By.XPATH, self.send_message).click()
         time.sleep(2)
         assert True == self.driver.find_element(By.XPATH, self.message_sent_success_msg).is_displayed()
+        print("SMS composed successfully!")
 
-    def send_broadcast(self):
+    def send_broadcast_message(self):
         self.driver.find_element(By.LINK_TEXT, self.broadcasts).click()
         time.sleep(2)
         self.driver.find_element(By.XPATH, self.add_broadcast).click()
         time.sleep(2)
         self.driver.find_element(By.XPATH, self.broadcast_name).send_keys("broadcast_" + fetch_random_string())
         self.driver.find_element(By.XPATH, self.recipients).click()
-        self.driver.find_element(By.XPATH, self.select_users_recipients).click()
+        self.driver.find_element(By.XPATH, self.select_recipient_type).click()
+        self.driver.find_element(By.XPATH, self.user_recipient).click()
+        time.sleep(2)
         self.driver.find_element(By.XPATH, self.select_value_dropdown).click()
-        self.driver.find_element(By.XPATH, self.broadcast_message).sennd_keys("Test Broadcast:" + "broadcast_"
+        self.driver.find_element(By.XPATH, self.broadcast_message).send_keys("Test Broadcast:" + "broadcast_"
                                                                               + fetch_random_string())
         self.driver.find_element(By.XPATH, self.send_broadcast).click()
         time.sleep(2)
         assert True == self.driver.find_element(By.XPATH, self.broadcast_created).is_displayed()
+        print("Broadcast created successfully!")
 
     def create_cond_alert(self):
         self.driver.find_element(By.LINK_TEXT, self.cond_alerts).click()
-        time.sleep(2)
         self.driver.find_element(By.LINK_TEXT, self.add_cond_alert).click()
-        time.sleep(2)
         self.driver.find_element(By.XPATH, self.cond_alert_name).send_keys("cond_alert_" + fetch_random_string())
         self.driver.find_element(By.XPATH, self.continue_button_basic_tab).click()
         self.driver.find_element(By.XPATH, self.case_type).click()
         self.driver.find_element(By.XPATH, self.case_type_option_value).click()
         self.driver.find_element(By.XPATH, self.continue_button_rule_tab).click()
         self.driver.find_element(By.XPATH, self.recipients).click()
-        self.driver.find_element(By.XPATH, self.select_users_recipients).click()
-        self.driver.find_element(By.XPATH, self.broadcast_message).sennd_keys("Test Alert:" + "cond_alert_"
+        self.driver.find_element(By.XPATH, self.select_recipient_type).click()
+        self.driver.find_element(By.XPATH, self.broadcast_message).send_keys("Test Alert:" + "cond_alert_"
                                                                               + fetch_random_string())
         self.driver.find_element(By.XPATH, self.send_message).click()
-        time.sleep(2)
         assert True == self.driver.find_element(By.XPATH, self.cond_alert_created).is_displayed()
+        print("Conditional Alert created successfully!")
 
     def cond_alert_download(self):
+        self.driver.find_element(By.LINK_TEXT, self.cond_alerts).click()
+        time.sleep(2)
         self.driver.find_element(By.LINK_TEXT, self.bulk_upload_button).click()
         self.driver.find_element(By.LINK_TEXT, self.download).click()
+        time.sleep(3)
+        print("Conditional Alert downloaded successfully!")
 
     def cond_alert_upload(self):
-        path = Path(str(Path.home()) + '\Downloads\*.xlsx')
+        path = Path(str(Path.home()) + '\\Downloads\\*.xlsx')
         list_of_files = glob.glob(str(path))
         latest_file = max(list_of_files, key=os.path.getctime)
         print(latest_file)
-        self.driver.find_element(By.LINK_TEXT, self.choose_file).send_keys(latest_file)
-        self.driver.find_element(By.LINK_TEXT, self.upload).click()
+        self.driver.find_element(By.XPATH, self.choose_file).send_keys(latest_file)
+        time.sleep(2)
+        self.driver.find_element(By.XPATH, self.upload).click()
+        time.sleep(2)
+        assert True == self.driver.find_element(By.XPATH, self.upload_success_message).is_displayed()
+        print("Conditional Alert uploaded successfully!")
 
-    def add_keyword(self):
+    def add_keyword_trigger(self):
         self.driver.find_element(By.LINK_TEXT, self.keywords).click()
         self.driver.find_element(By.LINK_TEXT, self.add_keyword).click()
+        time.sleep(2)
         self.driver.find_element(By.XPATH, self.keyword_name).send_keys("keyword_" + fetch_random_string())
         self.driver.find_element(By.XPATH, self.keyword_description).send_keys("keyword_" + fetch_random_string())
-        self.driver.find_element(By.XPATH, self.keyword_message).send_keys("Test Message"+"keyword_"
+        self.driver.find_element(By.XPATH, self.keyword_message).send_keys("Test Message: " + "keyword_"
                                                                            + fetch_random_string())
         self.driver.find_element(By.XPATH, self.send_message).click()
+        time.sleep(2)
         assert True == self.driver.find_element(By.XPATH, self.keyword_created).is_displayed()
+        print("Keyword created successfully!")
 
-    def add__structured_keyword(self):
-        self.driver.find_element(By.LINK_TEXT, self.keywords).click()
-        self.driver.find_element(By.LINK_TEXT, self.add__structured_keyword()).click()
-        self.driver.find_element(By.XPATH, self.keyword_name).send_keys("structured_keyword_" + fetch_random_string())
-        self.driver.find_element(By.XPATH, self.keyword_description).send_keys("structured_keyword_"
-                                                                               + fetch_random_string())
-        self.driver.find_element(By.XPATH, self.keyword_survey).click()
-        self.driver.find_element(By.XPATH, self.survey_option_select).click()
-        self.driver.find_element(By.XPATH, self.keyword_message).send_keys("Test Message"+"structured_keyword_"
-                                                                           + fetch_random_string())
-        self.driver.find_element(By.XPATH, self.send_message).click()
+    def add_structured_keyword_trigger(self):
+        WebDriverWait(self.driver, 10).until(ec.element_to_be_clickable((By.LINK_TEXT, self.keywords))).click()
+        WebDriverWait(self.driver, 10).until(ec.element_to_be_clickable((
+            By.LINK_TEXT, self.add_structured_keyword))).click()
+        WebDriverWait(self.driver, 10).until(ec.element_to_be_clickable((By.XPATH, self.keyword_name))).send_keys(
+            "structured_keyword_" + fetch_random_string())
+        WebDriverWait(self.driver, 10).until(ec.element_to_be_clickable((
+            By.XPATH, self.keyword_description))).send_keys("structured_keyword_" + fetch_random_string())
+        WebDriverWait(self.driver, 10).until(ec.element_to_be_clickable((By.XPATH, self.keyword_survey))).click()
+        WebDriverWait(self.driver, 10).until(ec.element_to_be_clickable((By.XPATH, self.survey_option_select))).click()
+        WebDriverWait(self.driver, 10).until(ec.element_to_be_clickable((By.XPATH, self.keyword_message))).send_keys(
+            "Test Message" + "structured_keyword_" + fetch_random_string())
+        WebDriverWait(self.driver, 10).until(ec.element_to_be_clickable((By.XPATH, self.send_message))).click()
+        time.sleep(2)
         assert True == self.driver.find_element(By.XPATH, self.structured_keyword_created).is_displayed()
+        print("Structured keyword created successfully!")
 
-    def chats(self):
+    def chat_page(self):
         self.driver.find_element(By.LINK_TEXT, self.chat).click()
         assert True == self.driver.find_element(By.ID, self.contact_table).is_displayed()
+        print("Chat Page loaded successfully!")
 
-    def sms_connectivity(self):
-        self.driver.find_element(By.LINK_TEXT, self.sms_connectivity).click()
-        self.driver.find_element(By.XPATH, self.add_gateway).click()
-        self.driver.find_element(By.XPATH, self.gateway_name).send_keys("gateway_" + fetch_random_string())
-        self.driver.find_element(By.XPATH, self.host_and_port).send_keys("gateway_" + fetch_random_string())
-        self.driver.find_element(By.XPATH, self.username).send_keys("gateway_" + fetch_random_string())
-        self.driver.find_element(By.XPATH, self.password).send_keys("gateway_" + fetch_random_string())
-        self.driver.find_element(By.XPATH, self.sender_id).send_keys("gateway_" + fetch_random_string())
-        self.driver.find_element(By.XPATH, self.client_name).send_keys("gateway_" + fetch_random_string())
-        self.driver.find_element(By.XPATH, self.campaign_name).send_keys("gateway_" + fetch_random_string())
-        time.sleep(2)
-        assert True == self.driver.find_element(By.XPATH, self.gateway_created).is_displayed()
+    # def sms_connectivity_gateway(self):
+    #     self.driver.find_element(By.LINK_TEXT, self.sms_connectivity).click()
+    #     time.sleep(2)
+    #     self.driver.find_element(By.XPATH, "//select[@name='hq_api_id']").click()
+    #     time.sleep(2)
+    #     self.driver.find_element(By.XPATH, "//select[@name='hq_api_id']/option[text()='Airtel (through TCL)']").click()
+    #     self.driver.find_element(By.XPATH, self.add_gateway).click()
+    #     self.driver.find_element(By.XPATH, self.gateway_name).send_keys("gateway_" + fetch_random_string())
+    #     self.driver.find_element(By.XPATH, self.host_and_port).send_keys("gateway_" + fetch_random_string())
+    #     self.driver.find_element(By.XPATH, self.username).send_keys("gateway_" + fetch_random_string())
+    #     self.driver.find_element(By.XPATH, self.password).send_keys("gateway_" + fetch_random_string())
+    #     self.driver.find_element(By.XPATH, self.sender_id).send_keys("gateway_" + fetch_random_string())
+    #     self.driver.find_element(By.XPATH, self.client_name).send_keys("gateway_" + fetch_random_string())
+    #     self.driver.find_element(By.XPATH, self.campaign_name).send_keys("gateway_" + fetch_random_string())
+    #     time.sleep(2)
+    #     assert True == self.driver.find_element(By.XPATH, self.gateway_created).is_displayed()
+    #     print("Gateway created successfully!")
 
-    def general_settings(self):
+    def general_settings_page(self):
         self.driver.find_element(By.LINK_TEXT, self.general_settings).click()
         time.sleep(2)
-        if self.driver.find_element(By.XPATH, self.disable_button).is_selected():
+        if self.driver.find_element(By.XPATH, self.disable_button).is_enabled():
             self.driver.find_element(By.XPATH, self.enable_button).click()
             self.driver.find_element(By.XPATH, self.time_input).send_keys("23:59")
         else:
             self.driver.find_element(By.XPATH, self.disable_button).click()
         self.driver.find_element(By.XPATH, self.send_message).click()
-        assert True == self.driver.find_element(By.XPATH, self.self.message_sent_success_msg).is_displayed()
+        time.sleep(2)
+        assert True == self.driver.find_element(By.XPATH, self.message_sent_success_msg).is_displayed()
+        print("Settings page updated successfully!")
 
-    def languages(self):
-        self.driver.find_element(By.LINK_TEXT, self.languages).click()
-        self.driver.find_element(By.XPATH, self.add_lang).click()
-        self.driver.find_element(By.XPATH, self.lang_input_textarea).click()
-        self.driver.find_element(By.XPATH, self.select_first_lang).click()
-        self.driver.find_element(By.XPATH, self.save_lang).click()
-        self.driver.find_element(By.XPATH, self.lang_input_textarea).click()
-        self.driver.find_element(By.XPATH, self.select_second_lang).click()
-        self.driver.find_element(By.XPATH, self.save_lang).click()
-        self.driver.find_element(By.XPATH, self.delete_lang).click()
-        self.driver.find_element(By.XPATH, self.save_lang).click()
-
-
-
-
-
-
-
-
-
-
-
-
-
+    def languages_page(self):
+        WebDriverWait(self.driver, 10).until(ec.element_to_be_clickable((By.LINK_TEXT, self.languages))).click()
+        WebDriverWait(self.driver, 10).until(ec.element_to_be_clickable((By.XPATH, self.add_lang))).click()
+        WebDriverWait(self.driver, 10).until(ec.element_to_be_clickable((By.XPATH, self.lang_input_textarea))).click()
+        WebDriverWait(self.driver, 10).until(ec.element_to_be_clickable((By.XPATH, self.select_first_lang))).click()
+        WebDriverWait(self.driver, 10).until(ec.element_to_be_clickable((By.XPATH, self.save_lang))).click()
+        WebDriverWait(self.driver, 10).until(ec.element_to_be_clickable((By.XPATH, self.lang_input_textarea))).click()
+        WebDriverWait(self.driver, 10).until(ec.element_to_be_clickable((By.XPATH, self.select_second_lang))).click()
+        WebDriverWait(self.driver, 10).until(ec.element_to_be_clickable((By.XPATH, self.save_lang))).click()
+        WebDriverWait(self.driver, 10).until(ec.element_to_be_clickable((By.XPATH, self.delete_lang))).click()
+        WebDriverWait(self.driver, 10).until(ec.element_to_be_clickable((By.XPATH, self.save_lang))).click()
+        print("Language added and deleted successfully!")
