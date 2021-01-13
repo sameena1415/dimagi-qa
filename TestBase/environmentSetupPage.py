14e1cb650f93daca648ad1376c870482e7878405
Daniel Miller <dmiller@dimagi.com>
2021-01-13 17:14:10 -0500

    New settings format

--- TestBase/environmentSetupPage.py
+++ TestBase/environmentSetupPage.py
@@ -1,8 +1,9 @@
 import unittest
+from configparser import ConfigParser
+from pathlib import Path
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from Pages.loginPage import LoginPage
-from UserInputs.loginCredentials import LoginCredentials
 
 
 class EnvironmentSetup(unittest.TestCase):
@@ -11,12 +12,14 @@ class EnvironmentSetup(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+        settings = load_settings()["default"]
+        driver_path = ChromeDriverManager().install()
+        cls.driver = webdriver.Chrome(executable_path=driver_path)
         cls.driver.maximize_window()
-        cls.driver.get(LoginCredentials.url)
+        cls.driver.get(settings["url"])
         login = LoginPage(cls.driver)
-        login.enter_username(LoginCredentials.login_username)
-        login.enter_password(LoginCredentials.login_password)
+        login.enter_username(settings["login_username"])
+        login.enter_password(settings["login_password"])
         login.click_submit()
         login.accept_alert()
         print("Successfully logged in")
@@ -28,5 +31,14 @@ class EnvironmentSetup(unittest.TestCase):
         print("Test Completed")
 
 
-if __name__ == "__main__":
-    unittest.main()
+def load_settings():
+    path = Path(__file__).parent.parent / "settings.cfg"
+    if not path.exists():
+        raise RuntimeError(
+            f"Not found: {path}\n\n"
+            "Copy settings-sample.cfg to settings.cfg and populate "
+            "it with values for the environment you want to test."
+        )
+    settings = ConfigParser()
+    settings.read(path)
+    return settings
