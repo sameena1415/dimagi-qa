1a68feb6ba28ba84f6c83bbdff3ef6e68db3cc43
kiran-tripathy <ktripathy@dimagi.com>
2021-01-22 12:45:49 +0530

    Added utility method to reduce duplication

--- Pages/homePage.py
+++ Pages/homePage.py
@@ -5,10 +5,6 @@ from selenium.webdriver.support import expected_conditions as ec
 
 class HomePage:
 
-    # self represents the instance of the class. By using the "self" keyword we can access the attributes and methods
-    # of the class in python.__init__ is used when an object is created from the class and it allow the class to
-    # initialize the attributes of a class.
-
     def __init__(self, driver):
         self.driver = driver
         self.dashboard_menu_id = "DashboardTab"
@@ -23,46 +19,33 @@ class HomePage:
         self.messaging_menu_id = "MessagingTab"
         self.admin_menu_id = "AdminTab"
 
+    def wait_to_click(self, *locator, timeout=3):
+        clickable = ec.element_to_be_clickable(locator)
+        WebDriverWait(self.driver, timeout).until(clickable).click()
+
     def dashboard_menu(self):
-        WebDriverWait(self.driver, 5).until(ec.element_to_be_clickable((
-            By.ID, self.dashboard_menu_id))).click()
+        self.wait_to_click(By.ID, self.dashboard_menu_id)
 
     def reports_menu(self):
-        WebDriverWait(self.driver, 5).until(ec.element_to_be_clickable((
-            By.ID, self.reports_menu_id))).click()
-        WebDriverWait(self.driver, 5).until(ec.element_to_be_clickable((
-            By.LINK_TEXT, self.view_all_link_text))).click()
+        self.wait_to_click(By.ID, self.reports_menu_id)
+        self.wait_to_click(By.LINK_TEXT, self.view_all_link_text)
 
     def data_menu(self):
-        WebDriverWait(self.driver, 5).until(ec.element_to_be_clickable((
-            By.ID, self.data_menu_id))).click()
-        WebDriverWait(self.driver, 5).until(ec.element_to_be_clickable((
-            By.LINK_TEXT, self.view_all_link_text))).click()
+        self.wait_to_click(By.ID, self.data_menu_id)
+        self.wait_to_click(By.LINK_TEXT, self.view_all_link_text)
 
     def applications_menu(self):
-        WebDriverWait(self.driver, 5).until(ec.element_to_be_clickable((
-            By.ID, self.applications_menu_id))).click()
-        WebDriverWait(self.driver, 5).until(ec.element_to_be_clickable((
-            By.LINK_TEXT, self.new_application_link_text))).click()
+        self.wait_to_click(By.ID, self.applications_menu_id)
+        self.wait_to_click(By.LINK_TEXT, self.new_application_link_text)
 
     def users_menu(self):
-        WebDriverWait(self.driver, 5).until(ec.element_to_be_clickable((
-            By.ID, self.users_menu_id))).click()
-        WebDriverWait(self.driver, 5).until(ec.element_to_be_clickable((
-            By.LINK_TEXT, self.view_all_link_text))).click()
-
-    def web_apps_menu(self):
-        WebDriverWait(self.driver, 5).until(ec.element_to_be_clickable((
-            By.ID, self.web_apps_menu_id))).click()
-        WebDriverWait(self.driver, 5).until(ec.element_to_be_clickable((
-            By.ID, self.show_full_menu_id))).click()
+        self.wait_to_click(By.ID, self.users_menu_id)
+        self.wait_to_click(By.LINK_TEXT, self.view_all_link_text)
 
     def messaging_menu(self):
-        WebDriverWait(self.driver, 5).until(ec.element_to_be_clickable((
-            By.ID, self.messaging_menu_id))).click()
-        WebDriverWait(self.driver, 5).until(ec.element_to_be_clickable((
-            By.LINK_TEXT, self.view_all_link_text))).click()
+        self.wait_to_click(By.ID, self.messaging_menu_id)
+        self.wait_to_click(By.LINK_TEXT, self.view_all_link_text)
 
-    # def admin_menu(self):
-    #     self.driver.find_element(By.ID, self.admin_menu_id).click()
-    #     self.driver.find_element(By.LINK_TEXT, self.view_all_link_text).click()
+    def web_apps_menu(self):
+        self.wait_to_click(By.ID, self.web_apps_menu_id)
+        self.wait_to_click(By.ID, self.show_full_menu_id)
