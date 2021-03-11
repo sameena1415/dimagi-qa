be3f96ad2c86be0d3df5599c4e5591e5233a0311
Kiran T <67914792+kiran-tripathy@users.noreply.github.com>
2021-03-11 12:49:25 +0530

    Update UserInputs/userInputsData.py
    Co-authored-by: Daniel Miller <dmiller@dimagi.com>

--- UserInputs/userInputsData.py
+++ UserInputs/userInputsData.py
@@ -4,8 +4,10 @@ from pathlib import Path
 class UserInputsData:
     application = "Village Health"
     domain = "qa-automation"
-    download_path = Path('~/Downloads').expanduser()
-    download_path_ci = r"/home/runner/work/dimagi-qa/dimagi-qa/"
+    if os.environ.get("CI") == "true":
+        download_path = Path("/home/runner/work/dimagi-qa/dimagi-qa/")
+    else:
+        download_path = Path('~/Downloads').expanduser()
     # Report names
     form_export_name = "CCHQ Smoke Tests Form Export"
     case_export_name = "CCHQ Smoke Tests Case Export"
