9fa66a24f06d0af8959b68fbd5889e180cecb354
kiran-tripathy <ktripathy@dimagi.com>
2021-07-19 19:29:47 +0530

    updated wait check cond

--- Pages/organisationStructurePage.py
+++ Pages/organisationStructurePage.py
@@ -178,7 +178,7 @@ class OrganisationStructurePage:
         self.driver.find_element(By.ID, "id_bulk_upload_file").send_keys(str(file_that_was_downloaded))
         time.sleep(2)
         self.wait_to_click(By.XPATH, self.upload)
-        assert WebDriverWait(self.driver, 30).until(ec.presence_of_element_located((
+        assert WebDriverWait(self.driver, 30).until(ec.visibility_of_element_located((
             By.XPATH, self.import_complete))).is_displayed()
         print("File uploaded successfully")
 
