511e9fe278d0149b133ea036ff8563dd4dc6b67f
kiran-tripathy <ktripathy@dimagi.com>
2021-08-06 18:26:26 +0530

    added time.sleep

--- Pages/mobileWorkersPage.py
+++ Pages/mobileWorkersPage.py
@@ -153,7 +153,7 @@ class MobileWorkerPage:
         self.wait_to_click(By.XPATH, self.mobile_worker_on_left_panel)
         time.sleep(1)
         self.search_user()
-        time.sleep(1)
+        time.sleep(2)
         self.driver.find_element(By.LINK_TEXT, self.username).click()
 
     def enter_value_for_created_user_field(self):
