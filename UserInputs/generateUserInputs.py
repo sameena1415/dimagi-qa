14d10b775583e813ccb6db586c898b5d6fe29444
kankana <kankana.bordoloi@gmail.com>
2021-09-24 12:20:17 +0530

    adding the vaccine solutioning code

--- UserInputs/generateUserInputs.py
+++
@@ -1,9 +0,0 @@
-import random
-import string
-
-chars = string.ascii_lowercase + string.digits
-random_string = ''.join(random.choices(chars, k=6))
-
-
-def fetch_random_string():
-    return random_string
