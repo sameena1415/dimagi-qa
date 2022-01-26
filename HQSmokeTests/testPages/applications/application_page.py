77ada2fa20ce187093c01f5abc50eccf185f4487
kiran-tripathy <ktripathy@dimagi.com>
2022-01-26 15:03:33 +0530

    added sleep

--- HQSmokeTests/testPages/applications/application_page.py
+++ HQSmokeTests/testPages/applications/application_page.py
@@ -97,6 +97,7 @@ class ApplicationPage:
     def form_builder_exploration(self):
         time.sleep(2)
         self.driver.find_element(By.XPATH, self.menu_settings).click()
+        time.sleep(2)
         assert True == WebDriverWait(self.driver, 5).until(ec.presence_of_element_located((
             By.ID, self.menu_settings_content))).is_displayed()
         print("Menu Settings loaded successfully!")
