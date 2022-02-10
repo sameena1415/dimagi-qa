3bf887ead4cb710aaafa82778792e0a8ab8253ec
kiran-tripathy <ktripathy@dimagi.com>
2022-02-10 18:45:01 +0530

    uncommented code

--- HQSmokeTests/testCases/test_05_reports.py
+++ HQSmokeTests/testCases/test_05_reports.py
@@ -2,27 +2,27 @@ from HQSmokeTests.testPages.home.home_page import HomePage
 from HQSmokeTests.testPages.reports.report_page import ReportPage
 from HQSmokeTests.testPages.webapps.web_apps_page import WebAppsPage
 
-#
-# def test_case_14_report_loading(driver):
-#
-#     report = HomePage(driver)
-#     report.reports_menu()
-#     load = ReportPage(driver)
-#     load.worker_activity_report()
-#     load.daily_form_activity_report()
-#     load.submissions_by_form_report()
-#     load.form_completion_report()
-#     load.case_activity_report()
-#     load.completion_vs_submission_report()
-#     load.worker_activity_times_report()
-#     load.project_performance_report()
-#     load.submit_history_report()
-#     load.case_list_report()
-#     load.sms_usage_report()
-#     load.messaging_history_report()
-#     load.message_log_report()
-#     load.sms_opt_out_report()
-#     load.scheduled_messaging_report()
+
+def test_case_14_report_loading(driver):
+
+    report = HomePage(driver)
+    report.reports_menu()
+    load = ReportPage(driver)
+    load.worker_activity_report()
+    load.daily_form_activity_report()
+    load.submissions_by_form_report()
+    load.form_completion_report()
+    load.case_activity_report()
+    load.completion_vs_submission_report()
+    load.worker_activity_times_report()
+    load.project_performance_report()
+    load.submit_history_report()
+    load.case_list_report()
+    load.sms_usage_report()
+    load.messaging_history_report()
+    load.message_log_report()
+    load.sms_opt_out_report()
+    load.scheduled_messaging_report()
 
 
 def test_case_15_16_submit_form_verify_formdata_casedata(driver):
