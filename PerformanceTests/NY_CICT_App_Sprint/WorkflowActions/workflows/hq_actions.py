01b868b99cbfbb74fa39c066cbbceb2a7e46be4a
kiran-tripathy <ktripathy@dimagi.com>
2022-05-24 13:44:25 +0530

    added missing imports

--- PerformanceTests/NY_CICT_App_Sprint/WorkflowActions/workflows/hq_actions.py
+++ PerformanceTests/NY_CICT_App_Sprint/WorkflowActions/workflows/hq_actions.py
@@ -1,10 +1,12 @@
 import random
+import time
 
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 
 from PerformanceTests.NY_CICT_App_Sprint.WorkflowActions.base.base_page import BasePage
 from PerformanceTests.NY_CICT_App_Sprint.UserInputs.user_inputs import UserData
+from PerformanceTests.NY_CICT_App_Sprint.WorkflowActions.base.decorators import timer
 
 """"Contains test page elements and functions related to workflows in test"""
 
