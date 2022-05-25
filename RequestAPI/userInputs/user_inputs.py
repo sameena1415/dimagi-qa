2988f5aedd8456f4330420c515d32cf4cc9f4d86
Kankana Bordoloi <kankana.bordoloi@gmail.com>
2022-05-25 13:13:35 +0530

    updated ROOT path in user_input.py

--- RequestAPI/userInputs/user_inputs.py
+++ RequestAPI/userInputs/user_inputs.py
@@ -8,7 +8,7 @@ class UserData:
 
     """Path Settings"""
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    ROOT = os.path.abspath(os.pardir)
+    ROOT = os.path.abspath(os.pardir)+"/dimagi-qa"
     if os.environ.get("CI") == "true":
         DOWNLOAD_PATH = Path("/home/runner/work/dimagi-qa/dimagi-qa")
     else:
