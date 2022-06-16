0238624345a175155605ee8a31c061834d9441ca
Kankana Bordoloi <kankana.bordoloi@gmail.com>
2022-06-16 11:12:07 +0530

    adding a new QA Request folder for Dummy Data Generation for Data Engineer

---
+++ QA_Requests/DummyDataGenerationTaks/userInputs/test_data.py
@@ -0,0 +1,12 @@
+class TestData:
+    output_path = "./OutputFiles"
+    output_csv = "./OutputFiles/Records.csv"
+    output_xlsx = "./OutputFiles/Records.xlsx"
+    file_name = "Records.xlsx"
+    col_start = 1
+    col_end = 1950
+    case_type = "patient"
+    app_type = "Applications"
+    benchmark_application = "Data Engineer Interview"
+    case_list_name = "Case List"
+    form_name = "Register Patient"
