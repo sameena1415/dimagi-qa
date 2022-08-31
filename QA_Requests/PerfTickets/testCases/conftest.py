049d17c209b8a01ad81dffdd8a9497a9cb629918
kiran-tripathy <ktripathy@dimagi.com>
2022-08-31 15:20:30 +0530

    moved qa requests common fixtures to common utilities

--- QA_Requests/PerfTickets/testCases/conftest.py
+++ QA_Requests/PerfTickets/testCases/conftest.py
@@ -1,9 +1,7 @@
 from configparser import ConfigParser
 from pathlib import Path
 
-import pytest
-from QA_Requests.PerfTickets.userInputs.user_inputs import UserData
-from QA_Requests.PerfTickets.testPages.base.login_page import LoginPage
+from common_utilities.fixtures import *
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
@@ -30,7 +28,7 @@ def driver(load_settings):
     chrome_options.add_argument('--safebrowsing-disable-download-protection')
     chrome_options.add_argument('--safebrowsing-disable-extension-blacklist')
     chrome_options.add_experimental_option("prefs", {
-        "download.default_directory": str(UserData.DOWNLOAD_PATH),
+        "download.default_directory": str(PathSettings.DOWNLOAD_PATH),
         "download.prompt_for_download": False,
         "safebrowsing.enabled": True})
     driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), options=chrome_options)
@@ -39,26 +37,3 @@ def driver(load_settings):
     login.login(load_settings["login_username"], load_settings["login_password"])
     yield driver
     driver.quit()
-
-
-@pytest.hookimpl(hookwrapper=True)
-def pytest_runtest_makereport(item):
-    pytest_html = item.config.pluginmanager.getplugin("html")
-    outcome = yield
-    report = outcome.get_result()
-    extra = getattr(report, 'extra', [])
-    if report.when == "call" or report.when == "teardown":
-        xfail = hasattr(report, 'wasxfail')
-        if (report.skipped and xfail) or (report.failed and not xfail):
-            print("reports skipped or failed")
-            file_name = report.nodeid.replace("::", "_") + ".png"
-            screen_img = _capture_screenshot(item.funcargs["driver"])
-            if file_name:
-                html = '<div><img src="data:image/png;base64,%s" alt="screenshot" style="width:600px;height:300px;" ' \
-                       'onclick="window.open(this.src)" align="right"/></div>' % screen_img
-                extra.append(pytest_html.extras.html(html))
-        report.extra = extra
-
-
-def _capture_screenshot(driver):
-    return driver.get_screenshot_as_base64()
