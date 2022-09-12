eb3b6f27a97442edd398dcb7635b730f1262faf6
kiran-tripathy <ktripathy@dimagi.com>
2022-09-12 16:12:10 +0530

    added see more workflow

--- Features/CaseSearch/test_cases/test_01_webapps_workflows.py
+++ Features/CaseSearch/test_cases/test_01_webapps_workflows.py
@@ -49,7 +49,6 @@ def test_case_02_normal_workflow_search_again(driver):
 def test_case_03_search_first_search_all(driver):
     webapps = WebApps(driver)
     """Checks if user can submit a form"""
-    webapps.login_as('automation-user-1')
     webapps.open_app(case_search_app_name)
     webapps.open_menu("Songs (Search First)")
     webapps.search_all_cases_on_case_search_page()
@@ -62,10 +61,39 @@ def test_case_03_search_first_search_all(driver):
 def test_case_04_search_first_search_again(driver):
     webapps = WebApps(driver)
     """Checks if user can submit a form"""
-    webapps.login_as('automation-user-1')
     webapps.open_app(case_search_app_name)
     webapps.open_menu("Songs (Search First)")
     webapps.search_all_cases_on_case_search_page()
+    webapps.search_again_cases()
+    webapps.search_all_cases_on_case_search_page()
+    case_name = webapps.omni_search("Bugs")
+    webapps.select_case(case_name)
+    webapps.open_form("Play Song")
+    webapps.submit_the_form()
+
+
+def test_case_05_see_more_search_all(driver):
+    webapps = WebApps(driver)
+    casesearch = CaseSearchWorkflows(driver)
+    """Checks if user can submit a form"""
+    webapps.open_app(case_search_app_name)
+    webapps.open_menu("Songs (See More)")
+    webapps.search_all_cases()
+    casesearch.check_default_rating_on_caselist("5")
+    case_name = webapps.omni_search("Bugs")
+    webapps.select_case(case_name)
+    webapps.open_form("Play Song")
+    webapps.submit_the_form()
+
+
+def test_case_06_see_more_search_again(driver):
+    webapps = WebApps(driver)
+    casesearch = CaseSearchWorkflows(driver)
+    """Checks if user can submit a form"""
+    webapps.open_app(case_search_app_name)
+    webapps.open_menu("Songs (See More)")
+    webapps.search_all_cases()
+    casesearch.check_default_rating_on_caselist("5")
     webapps.search_again_cases()
     webapps.search_all_cases_on_case_search_page()
     case_name = webapps.omni_search("Bugs")
--- Features/CaseSearch/test_pages/casesearch_page.py
+++ Features/CaseSearch/test_pages/casesearch_page.py
@@ -10,15 +10,14 @@ class CaseSearchWorkflows(BasePage):
     def __init__(self, driver):
         super().__init__(driver)
 
-        self.username_in_table = (By.XPATH, "//td[@class='module-case-list-column'][last()]")
+        self.rating_in_table = (By.XPATH, "//td[@class='module-case-list-column'][4]")
         self.case_name_format = "//tr[.//td[text()='{}']]"
-
-    # def get_usernames_on_caselist(self, logged_in_user):
-    #     usernames = self.find_elements_texts(self.username_in_table)
-    #     assert logged_in_user in usernames
 
     def check_element_claimed(self, case_name):
         self.case = self.get_element(self.case_name_format, case_name)
         assert self.is_visible_and_displayed(self.case)
 
+    def check_default_rating_on_caselist(self, default_rating):
+        ratings = self.find_elements_texts(self.rating_in_table)
+        assert default_rating in ratings
 
