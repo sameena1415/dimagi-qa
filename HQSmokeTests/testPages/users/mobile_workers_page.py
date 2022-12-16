e1d74c39bbb706d22a496a217248a1d65d404dea
Kankana Bordoloi <kankana.bordoloi@gmail.com>
2022-12-16 13:43:43 +0530

    QA-4576 updated steps in mobile_workers_page.py

--- HQSmokeTests/testPages/users/mobile_workers_page.py
+++ HQSmokeTests/testPages/users/mobile_workers_page.py
@@ -216,7 +216,9 @@ class MobileWorkerPage(BasePage):
         self.send_keys(self.label_xpath, label)
 
     def add_choice(self, choice):
-        self.wait_to_click(self.choices_button_xpath)
+        self.wait_for_element(self.add_choice_button_xpath)
+        if self.is_present(self.choices_button_xpath):
+            self.wait_to_click(self.choices_button_xpath)
         self.wait_to_click(self.add_choice_button_xpath)
         self.clear(self.choice_xpath)
         self.send_keys(self.choice_xpath, choice)
