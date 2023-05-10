614e344c51a63123204c0e01ad7b0135dc2c69a0
Kankana Bordoloi <kankana.bordoloi@gmail.com>
2023-05-10 21:11:43 +0530

    QA-5025 commented all tags

--- HQSmokeTests/testCases/test_03_exports.py
+++ HQSmokeTests/testCases/test_03_exports.py
@@ -5,8 +5,8 @@ from HQSmokeTests.testPages.data.export_data_page import ExportDataPage
 """"Contains test cases related to the Exports module"""
 
 
-@pytest.mark.data
-@pytest.mark.exportsFormData
+# @pytest.mark.data
+# @pytest.mark.exportsFormData
 def test_case_21_form_exports(driver):
     export = ExportDataPage(driver)
     export.data_tab()
@@ -14,8 +14,8 @@ def test_case_21_form_exports(driver):
     export.form_exports()
 
 
-@pytest.mark.data
-@pytest.mark.exportsCaseData
+# @pytest.mark.data
+# @pytest.mark.exportsCaseData
 def test_case_21_case_exports(driver):
     export = ExportDataPage(driver)
     export.data_tab()
@@ -23,18 +23,18 @@ def test_case_21_case_exports(driver):
     export.case_exports()
 
 
-@pytest.mark.data
-@pytest.mark.exportsSMSMessages
+# @pytest.mark.data
+# @pytest.mark.exportsSMSMessages
 def test_case_22_sms_exports(driver):
     export = ExportDataPage(driver)
     export.data_tab()
     export.sms_exports()
 
 
-@pytest.mark.data
-@pytest.mark.exportsCaseData
-@pytest.mark.exportsFormData
-@pytest.mark.dailySavedExports
+# @pytest.mark.data
+# @pytest.mark.exportsCaseData
+# @pytest.mark.exportsFormData
+# @pytest.mark.dailySavedExports
 def test_case_24_daily_saved_exports(driver):
     export = ExportDataPage(driver)
     export.data_tab()
@@ -43,8 +43,8 @@ def test_case_24_daily_saved_exports(driver):
     export.daily_saved_exports_case()
 
 
-@pytest.mark.data
-@pytest.mark.deleteBulkExports
+# @pytest.mark.data
+# @pytest.mark.deleteBulkExports
 def test_exports_cleanup(driver):
     export = ExportDataPage(driver)
     export.data_tab()
