b229b5dcf528ed543f4055020ac6d48444ef5704
kiran-tripathy <ktripathy@dimagi.com>
2023-05-23 12:45:33 +0530

    added new directory structure for CO BHA tests

---
+++ USH_Apps/CO_BHA/README.md
@@ -0,0 +1,50 @@
+## CO BHA Regression Test Script
+
+This script contains covers the happy paths/critical workflows of Colorado's BHA app. Here are the scripted [regression tests.](https://docs.google.com/spreadsheets/d/1OIcd1V8Vd73OSPEt4x2o8N9MihqBlviAIvPbJAm2L4o/edit#gid=1373088023)
+
+## Executing Scripts
+
+### <ins> On Local Machine </ins>
+
+#### Setting up test environment
+
+```sh
+
+# create and activate a virtualenv using your preferred method. Example:
+python -m venv venv
+source venv/bin/activate
+
+
+# install requirements
+pip install -r requires.txt
+
+```
+
+[More on setting up virtual environments](https://confluence.dimagi.com/display/GTD/QA+and+Python+Virtual+Environments)
+
+
+#### Running Tests
+
+
+ -   Copy `settings-sample.cfg` to `settings.cfg` and populate `settings.cfg` for
+the environment you want to test.
+- Run tests using pytest command like:
+
+```sh
+
+# To execute all the test cases 
+pytest -v --rootdir= USH_Apps/CO_BHA/test_cases
+
+```
+- You could also pass the following arguments
+  - ` -n 3 --dist=loadfile` - This will run the tests parallelly in 3 instances. The number of reruns is configurable.
+  - ` --reruns 1` - This will re-run the tests once in case of failures.The number of reruns is configurable too.
+
+### <ins> Trigger Manually on Gitaction </ins>
+
+To manually trigger the script,
+  - Go to [CO BHA Tests action](https://github.com/dimagi/dimagi-qa/actions/workflows/co-bha-tests.yml)
+  - Run workflow
+  - Use workflow from ```master```
+  - Use the environment as desired
+  - Run!
---
+++ USH_Apps/CO_BHA/test_cases/conftest.py
@@ -0,0 +1,61 @@
+import os
+
+from configparser import ConfigParser
+from pathlib import Path
+from common_utilities.fixtures import *
+
+""""This file provides fixture functions for driver initialization"""
+
+global driver
+
+
+@pytest.fixture(scope="session")
+def environment_settings_casesearch():
+    """Load settings from os.environ
+
+            Names of environment variables:
+                DIMAGIQA_URL
+                DIMAGIQA_LOGIN_USERNAME
+                DIMAGIQA_LOGIN_PASSWORD
+
+            See https://docs.github.com/en/actions/reference/encrypted-secrets
+            for instructions on how to set them.
+            """
+    settings = {}
+    for name in ["url", "login_username", "login_password"]:
+
+        var = f"DIMAGIQA_{name.upper()}"
+        if var in os.environ:
+            settings[name] = os.environ[var]
+    if "url" not in settings:
+        env = os.environ.get("DIMAGIQA_ENV") or "staging"
+        subdomain = "www" if env == "production" else env
+        # updates the url with the project domain while testing in CI
+        settings["url"] = f"https://{subdomain}.commcarehq.org/a/co-carecoordination-test/cloudcare/apps/v2/#apps"
+    return settings
+
+
+@pytest.fixture(scope="session", autouse=True)
+def settings(environment_settings_casesearch):
+    if os.environ.get("CI") == "true":
+        settings = environment_settings_casesearch
+        settings["CI"] = "true"
+        if any(x not in settings for x in ["url", "login_username", "login_password"]):
+            lines = environment_settings_casesearch.__doc__.splitlines()
+            vars_ = "\n  ".join(line.strip() for line in lines if "DIMAGIQA_" in line)
+            raise RuntimeError(
+                f"Environment variables not set:\n  {vars_}\n\n"
+                "See https://docs.github.com/en/actions/reference/encrypted-secrets "
+                "for instructions on how to set them."
+            )
+        return settings
+    path = Path(__file__).parent.parent / "settings.cfg"
+    if not path.exists():
+        raise RuntimeError(
+            f"Not found: {path}\n\n"
+            "Copy settings-sample.cfg to settings.cfg and populate "
+            "it with values for the environment you want to test."
+        )
+    settings = ConfigParser()
+    settings.read(path)
+    return settings["default"]
