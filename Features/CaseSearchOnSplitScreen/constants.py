50c5bf0235ad588a30a52b0454ada497593a7dff
kiran-tripathy <ktripathy@dimagi.com>
2023-06-16 11:52:17 +0530

    added another set of set cases for split screen

---
+++ Features/CaseSearchOnSplitScreen/constants.py
@@ -0,0 +1,25 @@
+# Property Types
+TEXT_INPUT = "TEXT_INPUT"
+COMBOBOX = "COMBOBOX"
+YES = "YES"
+NO = "NO"
+
+# Search formats
+combobox = "combobox"
+text = "text"
+HOME = "HOME"
+WORK = "WORK"
+
+# EOF NAV
+FORM = "FORM"
+PREV_MENU = "PREV_MENU"
+MENU = "MENU"
+FIRST_MENU = "FIRST_MENU"
+HOME_SCREEN = "HOME_SCREEN"
+
+# Answer Types
+textarea = "textarea"
+
+# Case Types
+SONG = "SONG"
+SHOW = "SHOW"
