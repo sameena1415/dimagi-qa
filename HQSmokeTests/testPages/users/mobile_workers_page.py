a31b36c8062658bfab6242700840725394d0f03f
Kankana Bordoloi <kankana.bordoloi@gmail.com>
2023-07-09 16:15:17 +0530

    QA-5360 updated xpath to be prod compatible

--- HQSmokeTests/testPages/users/mobile_workers_page.py
+++ HQSmokeTests/testPages/users/mobile_workers_page.py
@@ -27,7 +27,7 @@ class MobileWorkerPage(BasePage):
         self.phone_number = UserData.area_code + fetch_phone_number()
 
         self.username_link = "//a[./i[@class='fa fa-user']][strong[.='{}']]"
-        self.remove_choice_button = "(//input[contains(@data-bind,'value: slug')]//following::a[@class='btn btn-danger' and @data-toggle='modal'][1])[{}]//preceding::button[contains(@data-bind,'removeChoice')][1]"
+        self.remove_choice_button = "(//input[contains(@data-bind,'value: slug')]//following::a[@class='btn btn-danger' and @data-toggle='modal'][1])[{}]//preceding::*[contains(@data-bind,'removeChoice')][1]"
         self.confirm_user_field_delete = (
         By.XPATH, "(//a[.='Cancel']//following-sibling::button[@class='btn btn-danger'])[last()]")
         self.delete_user_field = "(//input[contains(@data-bind,'value: slug')]//following::a[@class='btn btn-danger' and @data-toggle='modal'][1])[{}]"
