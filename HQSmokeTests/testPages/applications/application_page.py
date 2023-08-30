a3d91e3c6c113870676735091b56e880b539bf29
Kankana Bordoloi <kankana.bordoloi@gmail.com>
2023-08-30 15:58:06 +0530

    QA-5338 updated steps for getting app code

--- HQSmokeTests/testPages/applications/application_page.py
+++ HQSmokeTests/testPages/applications/application_page.py
@@ -91,6 +91,7 @@ class ApplicationPage(BasePage):
         self.delete_form_confirm = (By.XPATH, "//div[./p[./strong[contains(text(),'Android')]]]/following-sibling::div[button]//i[@class='fa fa-trash']")
         self.code = (By.XPATH, "//code")
         self.close = (By.XPATH, "//div[.//code]/following-sibling::div//a[contains(text(),'Close')]")
+        self.enter_app_code_link = (By.LINK_TEXT, "Enter App Code")
 
     def create_new_application(self):
         self.wait_to_click(self.applications_menu_id)
@@ -212,6 +213,7 @@ class ApplicationPage(BasePage):
         print("Sleeping for the installation code to generate")
         time.sleep(10)
         self.wait_to_click(self.publish_button)
+        self.wait_to_click(self.enter_app_code_link)
         code_text = self.wait_to_get_text(self.code)
         self.wait_to_click(self.close)
         # self.wait_to_click(self.delete_form)
