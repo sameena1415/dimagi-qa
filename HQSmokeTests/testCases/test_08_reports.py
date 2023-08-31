ecc6938843a80685603189cf5bccd7e3aa5f3d0e
Kankana Bordoloi <kankana.bordoloi@gmail.com>
2023-08-31 16:56:26 +0530

    QA-4874 resolved conflict

--- HQSmokeTests/testCases/test_08_reports.py
+++ HQSmokeTests/testCases/test_08_reports.py
@@ -102,6 +102,14 @@ def test_case_20_scheduled_report(driver, settings):
     load.verify_scheduled_report(time, user)
     load.delete_scheduled_and_saved_reports()
 
+@pytest.mark.report
+@pytest.mark.editReport
+@pytest.mark.p1p2EscapeDefect
+def test_case_72_edit_report(driver, settings):
+    report = HomePage(driver, settings)
+    report.reports_menu()
+    load = ReportPage(driver)
+    load.configure_add_report()
 
 @pytest.mark.report
 @pytest.mark.deleteReport
