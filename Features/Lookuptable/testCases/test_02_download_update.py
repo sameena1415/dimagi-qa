2d76f7932bb92c4bf9095ca9385395bda7c9c50d
Kankana Bordoloi <kankana.bordoloi@gmail.com>
2023-10-19 15:03:26 +0530

    QA-64 updated parameter in testcase 16

--- Features/Lookuptable/testCases/test_02_download_update.py
+++ Features/Lookuptable/testCases/test_02_download_update.py
@@ -166,8 +166,7 @@ def test_case_16_Attributes_1(driver, settings):
     print("path is ", download_path)
     home.data_menu()
     data.attribute_2(download_path, value)
-    driver.get(settings["url"]+UserData.url+settings['login_username'])
-    data.restore_attribute_1()
+    data.restore_attribute_1(settings["url"]+UserData.url+settings['login_username'])
     home.data_menu()
     data.delete_lookup_table(value)
 
