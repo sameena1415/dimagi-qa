b60a24104d66c2e972dd139a719ec6d1f219a6c6
Jonathan Tang <Jonahtang738@gmail.com>
2024-04-22 10:05:50 -0700

    refactor: parse arguments and clean up set up

--- LocustScripts/commcarehq-referrals-outgoing-referrals.py
+++ LocustScripts/commcarehq-referrals-outgoing-referrals.py
@@ -7,55 +7,32 @@ import random
 import json
 
 from collections import defaultdict
-from locust import HttpUser, SequentialTaskSet, between, task, tag, TaskSet
+from locust import HttpUser, SequentialTaskSet, between, task, tag, events
+from locust.exception import InterruptTaskSet
 from lxml import etree
 from datetime import datetime
 
+from user.models import UserDetails, HQUser, AppDetails
+from common.args import file_path
+from common.utils import load_json_data
 
+@events.init_command_line_parser.add_listener
+def _(parser):
+    parser.add_argument("--domain", help="CommCare domain", required=True, env_var="COMMCARE_DOMAIN")
+    parser.add_argument("--app-id", help="CommCare app id", required=True, env_var="COMMCARE_APP_ID")
+    parser.add_argument("--app-config", help="Configuration of CommCare app", required=True)
+    parser.add_argument("--user-details", help="Path to user details file", required=True)
+
+APP_CONFIG = {}
+USERS_DETAILS = []
 class WorkloadModelSteps(SequentialTaskSet):
     wait_time = between(5, 15)
 
     def on_start(self):
-        # get domain user credential and app config info
-        with open(self.user.app_config) as json_file:
-            data = json.load(json_file)
-            self.FUNC_OUTGOING_REFERRALS_MENU = data["FUNC_OUTGOING_REFERRALS_MENU"]
-            self.FUNC_ENTER_STATUS = data["FUNC_ENTER_STATUS"]
-            self.FUNC_ENTER_GENDER = data["FUNC_ENTER_GENDER"]
-            self.FUNC_ENTER_TYPE_OF_CARE = data["FUNC_ENTER_TYPE_OF_CARE"]
-
-        self._log_in()
-        self._get_build_info()
-
-    # noinspection PyUnusedLocal
-    def _log_in(self):
-        logging.info("_log_in - mobile worker: " + self.user.login_as)
-        login_url = f'/a/{self.user.domain}/login/'
-        response = self.client.get(login_url)
-        response = self.client.post(
-            login_url,
-            {
-                "auth-username": self.user.username,
-                "auth-password": self.user.password,
-                "cloud_care_login_view-current_step": ['auth'],  # fake out two_factor ManagementForm
-            },
-            headers={
-                "X-CSRFToken": self.client.cookies.get('csrftoken'),
-                "REFERER": f'{self.user.host}{login_url}',  # csrf requires this for secure requests
-            },
-        )
-        assert (response.status_code == 200)
-        assert ('Sign In' not in response.text)  # make sure we weren't just redirected back to login
-
-    def _get_build_info(self):
-        logging.info("_get_build_info - mobile worker: " + self.user.login_as)
-        response = self.client.get(f'/a/{self.user.domain}/cloudcare/apps/v2/?option=apps', name='build info')
-        assert (response.status_code == 200)
-        for app in response.json():
-            if app['copy_of'] == self.user.app_id:
-                # get build_id
-                self.build_id = app['_id']
-        logging.info("build_id: " + self.build_id)
+        self.FUNC_OUTGOING_REFERRALS_MENU = APP_CONFIG["FUNC_OUTGOING_REFERRALS_MENU"]
+        self.FUNC_ENTER_STATUS = APP_CONFIG["FUNC_ENTER_STATUS"]
+        self.FUNC_ENTER_GENDER = APP_CONFIG["FUNC_ENTER_GENDER"]
+        self.FUNC_ENTER_TYPE_OF_CARE = APP_CONFIG["FUNC_ENTER_TYPE_OF_CARE"]
 
     @tag('outgoing_referrals_menu')
     @task
@@ -146,55 +123,49 @@ class WorkloadModelSteps(SequentialTaskSet):
                     response.failure("error::data['" + checkKey + "'] != " + checkValue)
         return response.json()
 
-
+@events.init.add_listener
+def _(environment, **kw):
+    try:
+        app_config_path = file_path(environment.parsed_options.app_config)
+        APP_CONFIG.update(load_json_data(app_config_path))
+        logging.info("Loaded app config")
+    except Exception as e:
+        logging.error("Error loading app config: %s", e)
+        raise InterruptTaskSet from e
+    try:
+        user_path = file_path(environment.parsed_options.user_details)
+        user_data = load_json_data(user_path)["user"]
+        USERS_DETAILS.extend([UserDetails(**user) for user in user_data])
+        logging.info("Loaded %s users", len(USERS_DETAILS))
+    except Exception as e:
+        logging.error("Error loading users: %s", e)
+        raise InterruptTaskSet from e
 class LoginCommCareHQWithUniqueUsers(HttpUser):
     tasks = [WorkloadModelSteps]
-
-    formplayer_host = "/formplayer"
-    project = 'bha-referrals-perf'  # str(os.environ.get("project"))
-    domain_user_credential_force = str(os.environ.get("user_credential"))
-    app_config_force = str(os.environ.get("app_config"))
-    wait_time_force = "test"
-
-    if wait_time_force == "test":
-        wait_time = between(5, 10)
-
-    else:
-        wait_time = between(45, 90)
-
-    with open("project-config/" + project + "/config.yaml") as f:
-        config = yaml.safe_load(f)
-        host = config['host']
-        domain = config['domain']
-        app_id = config['app_id']
-        if domain_user_credential_force != "None":
-            domain_user_credential = "project-config/" + project + "/" + domain_user_credential_force
-        else:
-            domain_user_credential = config['domain_user_credential']
-        if app_config_force != "None":
-            app_config = "project-config/" + project + "/" + app_config_force
-        else:
-            app_config = config['app_config']
-
-    # get domain user credential and app config info
-    with open(domain_user_credential) as json_file:
-        data = json.load(json_file)
-        data_user = data['user']
+    wait_time = between(5, 10)
 
     def on_start(self):
-        now = datetime.now()
-        timestamp = datetime.timestamp(now)
-        dt_object = datetime.fromtimestamp(timestamp)
-        user_info = self.data_user.pop()
-        self.username = user_info['username']
-        self.password = user_info['password']
-        self.login_as = user_info['login_as']
-        print("userinfo===>>" + str(user_info))
+        self.domain = self.environment.parsed_options.domain
+        self.host = self.environment.parsed_options.host
+        self.user_detail = USERS_DETAILS.pop()
+        self.HQ_user = HQUser( self.user_detail)
+        logging.info("userinfo-->>>" + str(self.user_detail))
 
-        logging.info("timestamp-->>>" + str(dt_object))
-        logging.info("host-->>>" + self.host)
-        logging.info("login_as-->>>" + self.login_as)
-        logging.info("username-->>>" + self.username)
-        logging.info("domain-->>>" + self.domain)
-        logging.info("domain_user_credential-->>>" + self.domain_user_credential)
-        logging.info("app_config-->>>" + self.app_config)
+        self.login()
+        self.app_details = AppDetails(
+        domain = self.domain,
+        app_id = self.environment.parsed_options.app_id,
+        build_id = self._get_build_info(self.environment.parsed_options.app_id)
+        )
+
+    def login(self):
+        self.HQ_user.login(self.domain, self.host, self.client)
+
+    def _get_build_info(self, app_id):
+        response = self.client.get(f'/a/{self.domain}/cloudcare/apps/v2/?option=apps', name='build info')
+        assert (response.status_code == 200)
+        for app in response.json():
+            if app['copy_of'] == app_id:
+                # get build_id
+                logging.info("build_id: " + app['_id'])
+                return app['_id']
