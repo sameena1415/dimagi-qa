606c8e6bb9fb0898c4b6ed1986fd058c3ee972a9
Kankana Bordoloi <kbordoloi@dimagi.com>
2025-06-28 19:38:13 +0530

    Update manage_forms_page.py

--- HQSmokeTests/testPages/data/manage_forms_page.py
+++ HQSmokeTests/testPages/data/manage_forms_page.py
@@ -1,5 +1,6 @@
 import time
 
+from selenium.webdriver import ActionChains, Keys
 from selenium.webdriver.common.by import By
 
 from common_utilities.selenium.base_page import BasePage
@@ -30,16 +31,17 @@ class ManageFormsPage(BasePage):
         self.apply = (By.XPATH, "//button[@class='applyBtn btn btn-sm btn-primary']")
         self.date_range_manage_forms = (By.ID, "filter_range")
         self.users_field = (By.XPATH, "(//textarea[@class='select2-search__field'])[1]")
-        self.users_list_item = "//ul[@role='listbox']/li[contains(.,'{}')]"
+        self.users_list_item = "//ul[@role='listbox']/li[contains(.,'{}')][1]"
         self.village_app = (By.XPATH, "//option[text()='Village Health']")
         self.select_archive_restore = (By.XPATH, "//select[@name='archive_or_restore']")
         self.check_data = (By.XPATH, "//tr[@class = 'form-data-question ']")
+        self.remove_buttons = (By.XPATH, "//ul//button")
 
     def get_normal_forms(self, url=None):
         self.wait_and_sleep_to_click(self.manage_forms_link)
 
         # Date Filter
-        self.wait_for_element(self.date_range_manage_forms, 30)
+        self.wait_for_element(self.date_range_manage_forms, 150)
         self.wait_and_sleep_to_click(self.date_range_manage_forms)
         self.select_by_value(self.select_archive_restore, "archive")
         self.clear(self.date_range_manage_forms)
@@ -49,9 +51,10 @@ class ManageFormsPage(BasePage):
             self.send_keys(self.date_range_manage_forms, UserData.eu_date_having_submission)
         else:
             self.send_keys(self.date_range_manage_forms, UserData.india_date_having_submission)
-        self.send_keys(self.users_field, UserData.web_user)
-        self.wait_to_click((By.XPATH, self.users_list_item.format(UserData.web_user)))
-        self.wait_and_sleep_to_click(self.apply)
+        self.remove_default_users()
+        web_user = str(UserData.web_user).strip("[]")
+        self.send_keys(self.users_field, web_user)
+        self.wait_to_click((By.XPATH, self.users_list_item.format(web_user)), 30)
         # Report Apply
         self.wait_and_sleep_to_click(self.apply_button)
         time.sleep(2)
@@ -87,12 +90,16 @@ class ManageFormsPage(BasePage):
 
     def get_archived_forms(self):
         self.wait_and_sleep_to_click(self.manage_forms_link)
-        self.send_keys(self.users_field, UserData.web_user)
-        self.wait_to_click((By.XPATH, self.users_list_item.format(UserData.web_user)))
+        self.wait_for_element(self.date_range_manage_forms, 150)
+        self.remove_default_users()
+        web_user = str(UserData.web_user).strip("[]")
+        self.send_keys(self.users_field, web_user)
+        self.wait_to_click((By.XPATH, self.users_list_item.format(web_user)), 30)
         self.wait_and_sleep_to_click(self.archived_restored_dropdown)
         self.wait_and_sleep_to_click(self.archived_forms_option)
         self.wait_and_sleep_to_click(self.apply_button)
         self.reload_page()
+        self.wait_for_element(self.view_form_link, 200)
 
     def view_archived_forms(self):
         self.wait_and_sleep_to_click(self.view_form_link)
@@ -114,3 +121,17 @@ class ManageFormsPage(BasePage):
         self.wait_and_sleep_to_click(self.restore_button)
         assert self.is_present_and_displayed(self.success_message)
         print("Forms Restoration successful!!")
+        
+    def remove_default_users(self):
+        self.wait_for_element(self.users_field)
+        count = self.find_elements(self.remove_buttons)
+        print(len(count))
+        for i in range(len(count)):
+            count[0].click()
+
+            if len(count) != 1:
+                ActionChains(self.driver).send_keys(Keys.TAB).perform()
+
+            count = self.find_elements(self.remove_buttons)
+        ActionChains(self.driver).send_keys(Keys.ESCAPE).perform()
+
