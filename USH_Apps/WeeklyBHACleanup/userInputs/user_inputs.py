f3dac286127e33b846c380594cc547c2d68daa47
Kankana Bordoloi <kankana.bordoloi@gmail.com>
2025-07-19 14:34:05 +0530

    updated form name

--- USH_Apps/WeeklyBHACleanup/userInputs/user_inputs.py
+++ USH_Apps/WeeklyBHACleanup/userInputs/user_inputs.py
@@ -9,7 +9,7 @@ class UserData:
     # Pre-setup application and case names
     cr_app = "Automated Testing - CR"
     ccs_app = "Automated Testing - CCS"
-    cr_admit_client = "Admit Client"
+    cr_admit_client = "Admit Client."
     ccs_bed_avail = "Update Bed Availability"
     ccs_send_refferals = "Send Referrals"
     date_range = "Last 7 Days"
